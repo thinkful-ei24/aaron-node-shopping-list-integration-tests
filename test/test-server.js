8179850cbba3dddde865961c06f35ec75873e067
Aaron Whitehead <whitehead.aaron@live.com>
2018-09-14 12:33:39 -0700

    some small changes

--- test/test-server.js
+++ test/test-server.js
@@ -157,10 +157,10 @@ describe("Recipes", function() {
         })
 
         .then(function(res) {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.be.a("object");
-          expect(res.body).to.deep.equal(updateData);
+          expect(res).to.have.status(204);
+          // expect(res).to.be.json;
+          // expect(res.body).to.be.a("object");
+          // expect(res.body).to.deep.equal(updateData);
         })
     );
   });
